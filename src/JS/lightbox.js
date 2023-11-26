0f8f4122625d0db77f503149b0222828ffcc83a3
Igor-Principal <igorprincipal@gmail.com>
2023-11-26 19:24:16 +0200

    Update lightbox.js

--- src/JS/lightbox.js
+++ src/JS/lightbox.js
@@ -1,6 +1,5 @@
 // Описаний в документації
 import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export let lightbox = new SimpleLightbox('.img_wrap a', {
